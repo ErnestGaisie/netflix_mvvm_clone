0c8e2b937a6598e47717b4d7fcd2b0111eb4d306
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2022-12-13 17:40:11 +0000

    Creating custom coollection view cell

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -19,7 +19,7 @@ enum APIError: Error {
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void){
+    func getTrendingMovies(completion: @escaping (Result<[Title], Error>) -> Void){
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
@@ -28,10 +28,10 @@ class APICaller {
             }
             
             do{
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 completion(.success(results.results))
             }catch{
-                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
             
         }
@@ -39,7 +39,7 @@ class APICaller {
         task.resume()
     }
     
-    func getTrendingTvs(completion: @escaping (Result<[Tv], Error>) -> Void){
+    func getTrendingTvs(completion: @escaping (Result<[Title], Error>) -> Void){
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/tv/day?api_key=\(Constants.API_KEY)") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
@@ -48,12 +48,11 @@ class APICaller {
             }
             
             do{
-                let results = try JSONDecoder().decode(TrendingTvResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 print(results)
 //                completion(.success(results))
             }catch{
-                print(error)
-//                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
             
         }
@@ -62,7 +61,7 @@ class APICaller {
     }
     
     
-    func getUpcomingMovies(completion: @escaping (Result<[Movie], Error>) -> Void){
+    func getUpcomingMovies(completion: @escaping (Result<[Title], Error>) -> Void){
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/upcoming?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
@@ -71,12 +70,11 @@ class APICaller {
             }
             
             do{
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 print(results)
 //                completion(.success(results))
             }catch{
-                print(error)
-//                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
             
         }
@@ -85,7 +83,7 @@ class APICaller {
     }
     
     
-    func getPopular(completion: @escaping (Result<[Movie], Error>) -> Void){
+    func getPopular(completion: @escaping (Result<[Title], Error>) -> Void){
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/popular?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
@@ -94,12 +92,11 @@ class APICaller {
             }
             
             do{
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 print(results)
 //                completion(.success(results))
             }catch{
-                print(error)
-//                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
             
         }
@@ -107,7 +104,7 @@ class APICaller {
         task.resume()
     }
     
-    func getTopRated(completion: @escaping (Result<[Movie], Error>) -> Void){
+    func getTopRated(completion: @escaping (Result<[Title], Error>) -> Void){
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/top_rated?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
@@ -116,12 +113,11 @@ class APICaller {
             }
             
             do{
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 print(results)
 //                completion(.success(results))
             }catch{
-                print(error)
-//                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
             
         }
---
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by Gaisie on 2022/12/13.
+//
+
+import UIKit
+import SDWebImage
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identififer = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    public func configure(with model: String) {
+        guard let url = URL(string: model) else {return}
+        posterImageView.sd_setImage(with: url, completed: nil)
+    }
+    
+    
+}
