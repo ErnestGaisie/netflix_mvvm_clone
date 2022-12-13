2014047b592b4a33df316ec7e489f9c3c2469c4b
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2022-12-13 15:57:07 +0000

    Consuming API To Fetch Data for each Section

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -30,7 +30,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
-        getTrendingMovies()
+        fetchData()
     }
     
     private func configureNavBar () {
@@ -51,7 +51,7 @@ class HomeViewController: UIViewController {
         homeFeedTable.frame = view.bounds
     }
     
-    private func getTrendingMovies(){
+    private func fetchData(){
         APICaller.shared.getTrendingMovies{ results in
             switch results {
             case .success(let movies):
@@ -60,6 +60,23 @@ class HomeViewController: UIViewController {
                 print(error)
             }
         }
+        
+//        APICaller.shared.getTrendingTvs{ results in
+//
+//        }
+        
+//        APICaller.shared.getUpcomingMovies{ results in
+//
+//        }
+        
+//        APICaller.shared.getPopular{ results in
+//
+//        }
+        
+        APICaller.shared.getTopRated{ results in
+
+        }
+
     }
 }
 
