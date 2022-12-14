eac447fc14d452fd0927db3c821a13ce9315cde9
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2022-12-14 16:26:52 +0000

    Creating Title View Model

---
+++ Netflix Clone/ViewModels/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Netflix Clone
+//
+//  Created by Gaisie on 2022/12/14.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
